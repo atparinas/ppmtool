0836571464f432b0a9ab65c263b73f9f7d71d048
Andy Parinas <andy.parinas@gmail.com>
2019-06-12 22:01:52 +1000

    Entity Validations and Controller Validations

--- src/main/java/com/atparinas/ppmtool/domain/Project.java
+++ src/main/java/com/atparinas/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.atparinas.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,17 +14,27 @@ public class Project {
     @GeneratedValue
     private long id;
 
+    @NotBlank(message = "Project Name is required")
     private String projectName;
 
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min=4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
 
+    @NotBlank(message = "Description is required")
     private String description;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project() {
